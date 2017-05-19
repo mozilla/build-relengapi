532d68c68fad1c7b68b90169adfc3a52f1231b85
Rok Garbas <rok@garbas.si>
2017-05-19 13:57:21 +0200

    archiver: renaming tables and using its own connection

--- relengapi/blueprints/archiver/tables.py
+++ relengapi/blueprints/archiver/tables.py
@@ -5,8 +5,8 @@ import sqlalchemy as sa
 from relengapi.lib import db
 
 
-class ArchiverTask(db.declarative_base('relengapi')):
-    __tablename__ = 'archiver_tasks'
+class ArchiverTask(db.declarative_base('archiver')):
+    __tablename__ = 'releng_archiver_tasks'
     id = sa.Column(sa.Integer, primary_key=True)
     task_id = sa.Column(sa.String(100), nullable=False, unique=True)
     created_at = sa.Column(db.UTCDateTime(timezone=True), nullable=False)
